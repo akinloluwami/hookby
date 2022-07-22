b09ebb6d04d7151823ee23cff51d9c8cff51b483
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-22 15:01:57 +0100

    connected to supabase

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
-export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
