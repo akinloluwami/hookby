208e9183c431e64f6369dfc0e0cdde00a265fe8b
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-07-22 15:53:49 +0100

    create memehooks navbar components

---
+++ components/Navbar/memeHooks.ts
@@ -0,0 +1,20 @@
+const memeHooks = [
+  {
+    name: "useEat",
+    icon: "https://img.icons8.com/doodle/48/000000/pizza--v1.png",
+  },
+  {
+    name: "useSleep",
+    icon: "https://img.icons8.com/emoji/48/000000/sleeping-face.png",
+  },
+  {
+    name: "useCode",
+    icon: "https://img.icons8.com/color/48/000000/source-code.png",
+  },
+  {
+    name: "useRepeat",
+    icon: "https://img.icons8.com/parakeet/48/000000/experimental-repeat-parakeet.png",
+  },
+];
+
+export default memeHooks;
